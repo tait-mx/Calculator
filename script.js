73fd84544cbfc3dcede5cf54e8bdc36d4169ae3f
Ernesto Romero <ernestoromero@Mac-mini.local>
2026-05-15 13:51:33 -0600

    Create index.html and script.js
    Wrote the principal +, -, *, /  functions

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+console.log("Calculator project begins!")
+
+// Principal operation functions
+
+let add = function (a, b) { return a + b};
+let substract = function (a, b) { return a - b};
+let multiply = function (a, b) { return a * b};
+let divide = function (a, b) { return a / b};
+
+
+console.log("3 + 5 = " + add(3, 5));
+console.log("3 - 5 = " + substract(3, 5));
+console.log("3 * 5 = " + multiply(3, 5));
+console.log("3 / 5 = " + divide(3, 5));
